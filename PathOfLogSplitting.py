d4d3b36934861c0ce39a71d2f79f048d8ef2e226
Jayensee <118075978+Jayensee@users.noreply.github.com>
2024-04-14 00:58:28 +0100

    The log files are now saved in folders split by year

--- PathOfLogSplitting.py
+++ PathOfLogSplitting.py
@@ -1,4 +1,5 @@
 from os.path import exists
+from os import mkdir
 from time import time
 from json import load
 
@@ -10,6 +11,9 @@ with open("config.json", "r") as f:
 log_file_path = config["Original Log File Path"]
 output_folder_path = config["Output Folder Path"]
 
+if "Client.txt" not in log_file_path:
+    log_file_path += "/Client.txt"
+
 start_time = time()
 with open(log_file_path, "r", encoding="utf-8") as original_file:
     line = original_file.readline()
@@ -20,7 +24,11 @@ with open(log_file_path, "r", encoding="utf-8") as original_file:
             current_date = line[:10]
             raw_text += line
             if last_saved_date != current_date:
-                new_path = f"{output_folder_path}/{current_date.replace('/','_')}.txt"
+                year = current_date[:4]
+                new_folder = f"{output_folder_path}/{year}"
+                if not exists(new_folder):
+                    mkdir(new_folder)
+                new_path = f"{new_folder}/{current_date.replace('/','_')}.txt"
                 if not exists(new_path):
                     print(f"Saving {current_date.replace('/','_')}.txt", end="\r")
                     with open(new_path, "w", encoding="utf-8") as new_file:
